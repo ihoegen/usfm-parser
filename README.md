4dba80cbf9c8b4351d2430e97d5d48714a0c1ebd
Ian Hoegen <ianhoegen@gmail.com>
2016-08-25 10:50:28 -0700

    Lint files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # usfm-parser
+This library takes in USFM text, and outputs it into a JSON format.
+It also takes JSON formatted scripture and outputs it into USFM.
+## Setup
+`npm install usfm-parser`
+
+## Usage
+```js
+var usfm = require('usfm-parser');
+//Convert from USFM to JSON
+var toJSON = usfm.toJSON(/**USFM Text**/);
+//JSON to USFM
+var toUSFM = usfm.toUSFM(toJSON);
+```
--- src/js/jsonToUsfm.js
+++ src/js/jsonToUsfm.js
@@ -1,15 +1,25 @@
+/** ***********************************************************************
+ * @author Ian Hoegen
+ * @description Takes in scripture json and outputs it as a USFM string.
+ * @param {Object} json - Scripture in JSON
+ * @return {String} - Scripture in USFM
+*************************************************************************/
 exports.jsonToUSFM = function(json) {
   var final = [];
-  if(json.book) {
+  if (json.book) {
     final.push('\\h ' + json.book + '\n');
   }
-  for(var chapter in json.chapters) {
-    var currentChapter = json.chapters[chapter]
-    final.push('\\c ' + currentChapter.number + '\n\\p\n');
-    for (var verse in currentChapter.verses) {
-      var currentVerse = currentChapter.verses[verse]
-      final.push('\\v ' + currentVerse.number + ' ' + currentVerse.text);
+  for (var chapter in json.chapters) {
+    if (json.chapters.hasOwnProperty(chapter)) {
+      var currentChapter = json.chapters[chapter];
+      final.push('\\c ' + currentChapter.number + '\n\\p\n');
+      for (var verse in currentChapter.verses) {
+        if (currentChapter.verses.hasOwnProperty(verse)) {
+          var currentVerse = currentChapter.verses[verse];
+          final.push('\\v ' + currentVerse.number + ' ' + currentVerse.text);
+        }
+      }
     }
   }
   return final.join('');
-}
+};
